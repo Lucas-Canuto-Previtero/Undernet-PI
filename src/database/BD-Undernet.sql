037c2cd014dcf5e5b3c12fb6243ca292a2705e76
LCP05 <lucas.previtero@sptech.school>
2025-05-26 23:01:19 -0300

    ATUALIZAÇÃO - Mudança do script do banco de dados

--- src/database/BD-Undernet.sql
+++ src/database/BD-Undernet.sql
@@ -1,103 +1,100 @@
--- BANCO DE DADOS UNDERNET
-create database Undernet;
-Use Undernet;
+CREATE DATABASE undernet;
+USE undernet;
 
-Create table usuario (
-idUsuario int not null primary key auto_increment,
-nome varchar (50),
-email varchar (50),
-telefone char (11),
-senha varchar (30),
-fotoPerfil varchar (10000)
+
+
+CREATE TABLE usuario (
+  idUsuario INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50),
+  email VARCHAR(50),
+  telefone CHAR(11),
+  senha VARCHAR(30),
+  fotoPerfil VARCHAR(10000),
+  PRIMARY KEY (idUsuario)
+  );
+
+
+
+CREATE TABLE postagem (
+  idPostagem INT NOT NULL AUTO_INCREMENT,
+  fk_idUsuario INT NOT NULL,
+  dataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  texto VARCHAR(1000),
+  tipoPostagem VARCHAR(30),
+  imagem VARCHAR(10000),
+  PRIMARY KEY (idPostagem),
+  CONSTRAINT fkUsuarioPostagem FOREIGN KEY (fk_idUsuario) REFERENCES usuario (idUsuario)
+  );
+  
+  
+
+
+
+
+CREATE TABLE resultadoBadTime (
+  idResultadoBadTime INT NOT NULL AUTO_INCREMENT,
+  tempoPermanencia INT,
+  fk_idUsuario INT NOT NULL,
+  PRIMARY KEY (idResultadoBadTime),
+  CONSTRAINT fk_resultadobadtime_usuario1 FOREIGN KEY (fk_idUsuario) REFERENCES usuario (idUsuario)
 );
 
 
-Create table resultadoQuiz (
-idResultadoQuiz int not null auto_increment,
-fk_idUsuario int not null,
-dataHora timestamp default current_timestamp,
-tempoPermanencia int,
-acertos int,
-erros int,
-constraint fkUsuarioQuiz foreign key (fk_idUsuario) references usuario(idUsuario),
-Primary key (idResultadoQuiz)
+
+CREATE TABLE resultadoCorrida (
+  idResultadoCorrida INT NOT NULL AUTO_INCREMENT,
+  fk_idUsuario INT NOT NULL,
+  tempoPermanencia INT NULL DEFAULT NULL,
+  dataHora TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  CPS INT NULL DEFAULT NULL,
+  PRIMARY KEY (idResultadoCorrida),
+  CONSTRAINT fkUsuarioCorrida FOREIGN KEY (fk_idUsuario) REFERENCES usuario (idUsuario)
+  );
+
+
+
+CREATE TABLE resultadoPersona (
+  idResultadoPersona INT NOT NULL AUTO_INCREMENT,
+  dataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  tempoPermanencia INT,
+  chanceToriel INT,
+  chanceSans INT,
+  chancePapyrus INT,
+  chanceNapstablook INT,
+  chanceUndyne INT,
+  chanceFlowey INT,
+  chanceFrisk INT,
+  chanceChara INT,
+  chanceMettaton INT,
+  chanceAsgore INT,
+  chanceAsruel INT,
+  fk_idUsuario INT NOT NULL,
+  PRIMARY KEY (idResultadoPersona),
+  CONSTRAINT fk_resultadopersona_usuario1 FOREIGN KEY (fk_idUsuario) REFERENCES usuario (idUsuario)
 );
 
 
-Create table resultadoPersona (
-idResultadoPersona int not null auto_increment,
-fk_idUsuario int not null,
-dataHora timestamp default current_timestamp,
-tempoPermanencia int,
-chanceToriel int,
-chanceSans int,
-chancePapyrus int,
-chanceNapstablook int,
-chanceUndyne int,
-chanceFlowey int,
-chanceFrisk int,
-chanceChara int,
-chanceMettaton int,
-chanceAsgore int,
-chanceAsruel int,
-Constraint fkUsuarioTeste foreign key (fk_idUsuario) references usuario(idUsuario),
-Primary key (idResultadoPersona)
-);
-
-Create table resultadoPiano (
-idResultadoPiano int not null auto_increment,
-fk_idUsuario int not null,
-tempoPermanencia int,
-Constraint fkUsuarioPiano foreign key (fk_idUsuario) references usuario(idUsuario),
-Primary key (idResultadoPiano)
-);
-
-Create table resultadoBadTime (
-idResultadoBadTime int not null auto_increment,
-fk_idUsuario int not null,
-tempoPermanencia int,
-Constraint fkUsuarioBad foreign key (fk_idUsuario) references usuario(idUsuario),
-Primary key (idResultadoBadTime)
-);
+CREATE TABLE resultadoPiano (
+  idResultadoPiano INT NOT NULL AUTO_INCREMENT,
+  fk_idUsuario INT NOT NULL,
+  tempoPermanencia INT,
+  PRIMARY KEY (idResultadoPiano),
+  CONSTRAINT fkUsuarioPiano FOREIGN KEY (fk_idUsuario) REFERENCES usuario (idUsuario)
+  );
 
 
-Create table postagem (
-idPostagem int not null auto_increment,
-fk_idUsuario int not null,
-dataHora timestamp default current_timestamp,
-texto varchar(1000),
-tipoPostagem varchar(30),
-imagem varchar(10000),
-Constraint fkUsuarioPostagem foreign key (fk_idUsuario) references usuario(idUsuario),
-primary key (idPostagem)
-);
 
-Create table resultadoCorrida (
-idResultadoCorrida int not null auto_increment,
-fk_idUsuario int not null,
-tempoPermanencia int,
-dataHora timestamp default current_timestamp,
-CPS int,
-Constraint fkUsuarioCorrida foreign key (fk_idUsuario) references usuario(idUsuario),
-Primary key (idResultadoCorrida)
-);
+CREATE TABLE resultadoQuiz (
+  idResultadoQuiz INT NOT NULL AUTO_INCREMENT,
+  dataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  tempoPermanencia INT,
+  acertos INT,
+  erros INT,
+  fk_idUsuario INT NOT NULL,
+  PRIMARY KEY (idResultadoQuiz), 
+  CONSTRAINT fk_resultadoquiz_usuario1 FOREIGN KEY (fk_idUsuario) REFERENCES usuario (idUsuario)
+    );
 
-select * from Undernet.usuario;
-
+select * from resultadoBadTime;
+select * from usuario;
 select * from postagem;
-
- SELECT 
-            p.idPostagem,
-            p.texto AS descricao,
-            p.fk_idUsuario,
-            p.tipoPostagem,
-            p.imagem,
-            (SELECT DATE_FORMAT(p.dataHora, '%d/%m/%Y %H:%i:%s')) as dataHora_Formatada,
-            u.fotoPerfil,
-            u.idUsuario,
-            u.nome,
-            u.email,
-            u.senha
-        FROM postagem p
-            INNER JOIN usuario u
-                ON p.fk_idUsuario = u.idUsuario order by idPostagem desc;
